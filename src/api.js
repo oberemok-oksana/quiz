85936cc58f2d0f996626e68d98bf09be61fddcc7
Oksana Oberemok <Pylypenkok@gmail.com>
2022-10-21 19:19:50 +0800

    change link

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export const getQuestions = () => {
-  return fetch("https://opentdb.com/api.php?amount=5").then((response) =>
-    response.json()
+  return fetch("https://opentdb.com/api.php?amount=5&difficulty=medium").then(
+    (response) => response.json()
   );
 };
